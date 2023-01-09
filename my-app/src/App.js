bd5ef53969aa003fe5dee379a30c00e85c173ab5
clément moretti <98534268+daddykoala@users.noreply.github.com>
2023-01-09 16:20:08 +0100

    feature:parallax scroll

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,33 +8,34 @@ import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 function App() {
   return (
     <div className="App">
-      coucou
+      <div>normale</div>
+
     <Parallax pages={2} style={{ top: '0', left: '0' }} class="animation">
-      <ParallaxLayer offset={0} speed={2.5}>
-        <div className='animation-layer parallax' id='artback'></div>
+      <ParallaxLayer offset={0} speed={0.01}>
+        <div className='animation-layer parallax' id="artback"></div>
       </ParallaxLayer>
-      <ParallaxLayer offset={0} speed={2.5}>
+      <ParallaxLayer offset={0} speed={0.1}>
         <div className='animation-layer parallax' id='mountain'></div>
       </ParallaxLayer>
-      <ParallaxLayer offset={0} speed={2.5}>
+      <ParallaxLayer offset={0} speed={0.2}>
         <div className='animation-layer parallax' id='logoland'></div>
       </ParallaxLayer>
-      <ParallaxLayer offset={0} speed={2.5}>
+      <ParallaxLayer offset={0} speed={0.23}>
         <div className='animation-layer parallax' id='jungle1'></div>
       </ParallaxLayer>
-      <ParallaxLayer offset={0} speed={2.5}>
+      <ParallaxLayer offset={0} speed={0.30}>
         <div className='animation-layer parallax' id='jungle2'></div>
       </ParallaxLayer>
-      <ParallaxLayer offset={0} speed={2.5}>
+      <ParallaxLayer offset={0} speed={0.35}>
         <div className='animation-layer parallax' id='jungle3'></div>
       </ParallaxLayer>
-      <ParallaxLayer offset={0} speed={2.5}>
+      <ParallaxLayer offset={0} speed={0.4}>
         <div className='animation-layer parallax' id='jungle4'></div>
       </ParallaxLayer>
-      <ParallaxLayer offset={0} speed={2.5}>
+      <ParallaxLayer offset={0} speed={0.45}>
         <div className='animation-layer parallax' id='manonmountain'></div>
       </ParallaxLayer>
-      <ParallaxLayer offset={0} speed={2.5}>
+      <ParallaxLayer offset={0} speed={0.5}>
         <div className='animation-layer parallax' id='jungle5'></div>
       </ParallaxLayer>
     </Parallax>
